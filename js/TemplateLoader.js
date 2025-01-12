2d637dab458e910159db7f363eafde7da717dfa8
r3dsd <dogsheepduck@gmail.com>
2025-01-12 15:19:22 +0900

    fix: update option

--- js/TemplateLoader.js
+++ js/TemplateLoader.js
@@ -1,29 +1,42 @@
 import { app } from "../../../scripts/app.js";
 import { api } from "../../../scripts/api.js";
 
+const cache = {};
+
 async function getKeys(filename) {
+    if (caches[filename]) {
+        return caches[filename];
+    }
+    
     const res = await api.fetchApi(`/templates/${filename}`);
     const { keys } = await res.json();
-    return keys;
+    caches[filename] = keys;
+    return caches[filename];
 }
 
 app.registerExtension({
     name: "TemplateLoader",
-    async nodeCreated(node) {
-        const setKeys = async (node) => {
-            const keys = await getKeys(node.widgets[0].value);
-            Object.defineProperty(node.widgets[1].options, "values", {
+    nodeCreated(node) {
+        if (node.comfyClass == "TemplateLoader") {
+            const sourceWidget = node.widgets[node.widgets.findIndex(w => w.name == "source")];
+            const templateWidget = node.widgets[node.widgets.findIndex(w => w.name == "template")];
+
+            Object.defineProperty(templateWidget.options, "values", {
+                set:(x) => {},
                 get() {
-                    return keys;
+                    getKeys(sourceWidget.value);
+                    return caches[sourceWidget.value];
                 }
             });
-        }
-
-        if (node.comfyClass == "TemplateLoader") {
-            setKeys(node);
-            node.widgets[1].value = "Select the Template.";
-            node.widgets[0].callback = async () => {
-                const keys = setKeys(node);
+            
+            sourceWidget.callback = async () => {
+                const keys = await getKeys(sourceWidget.value);
+                Object.defineProperty(templateWidget.options, "values", {
+                    set:(x) => {},
+                    get() {
+                        return keys;
+                    }
+                });
                 node.widgets[1].value = keys[0] || "";
             }
         }
